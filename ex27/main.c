22fae7b8182edfaa3d0d01143c5ab08bad0a412b
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-10-08 16:23:23 -0300

    ex27 4th try

--- ex27/main.c
+++ ex27/main.c
@@ -6,7 +6,7 @@
 /*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 11:12:20 by jarao-de          #+#    #+#             */
-/*   Updated: 2024/10/08 16:10:44 by jarao-de         ###   ########.fr       */
+/*   Updated: 2024/10/08 16:20:42 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	invalid_arguments_count(int argc)
 	if (argc == 2)
 		return (0);
 	if (argc == 1)
-		write(2, "File name missing.\n", 20);
+		write(2, "File name missing.\n", 19);
 	if (argc > 2)
-		write(2, "Too many arguments.\n", 21);
+		write(2, "Too many arguments.\n", 20);
 	return (1);
 }
 
